52564e908eb46ab3513b84acddb3e705defff3f7
haeminK <112795904+haeminK@users.noreply.github.com>
2024-06-26 00:47:58 +0900

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,10 @@ const commentRoute = require('./routes/comment');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(process.env.MONGODB_API);
+    await mongoose.connect(process.env.MONGODB_API, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     console.log("database connected")
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
